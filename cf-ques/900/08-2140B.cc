cd67506672ee1e1c53532746728f07d8c450f5a2
mousammm <mxmssdia@gmail.com>
2026-02-07 00:16:29 +0530

    08

--- cf-ques/900/08-2140B.cc
+++ cf-ques/900/08-2140B.cc
@@ -7,10 +7,27 @@
  * x # y % x + y == 0
  *
  * sol:
- * x # y = x.10^d + y = x.(10^d - 1) + (x + y)
- * - for x # y to be divisible by x + y 
- * - x + y need to be a factor of x.(10^d - 1)
- *   3.x is a factor of x.(10^d - 1)
+ *   1/x # y:
+ *   x.10^d + y 
+ *   x.10^d + y + x - x
+ *   x.10^d + x + y - x
+ *   x.(10^d + 1) + y - x
+ *   or 
+ *   x.(10^d - 1) + y + x
+ *   x.10^d - x + y + x
+ *   x.10^d + y
+ *   so x # y = x(10^d - 1) + (x + y)
+ *
+ *   2/if m = x + y the m divides x(10^d - 1) + m
+ *   equivalent to m divides x(10^d - 1)
+ *   so x + y | x(10^d - 1)
+ *
+ *   3/why 3x is a factor of x(10^d - 1)
+ *   10d - 1 (d 1 to ...)
+ *   9, 99, 99, ...
+ *   so 3x divides 9, 99, 999, ...
+ *
+ *   4/ x + y = 3x
  *   x + y = 3.x
  *   y = 3.x - x
  *   y = 2.x
